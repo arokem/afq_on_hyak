3ea1e8857284f7d4de1d8b11f547826eb47cbc69
Ariel Rokem <arokem@gmail.com>
2023-03-18 21:29:14 -0700

    One more small thing.

--- script.py
+++ script.py
@@ -91,7 +91,7 @@ def afq_this(subject):
 
     for lpath in glob.glob(op.join(bids_path, f"derivatives/afq/sub-{subject}", "*", "*")):
         rpath = op.join(f"{bucket}/derivatives/afq{datetime.today().strftime('%Y-%m-%d')}/sub-{subject}/",
-                        op.split(lpath[0])[-1])
+                        op.split(lpath)[-1])
         print(f"Putting {lpath} in {rpath}")
         fs.put(lpath, rpath)
 
