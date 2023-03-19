0150a5bfb4c295e56adbaf6b32da09274fbed720
Ariel Rokem <arokem@gmail.com>
2023-03-18 20:20:55 -0700

    Can't get logs per task.

--- script.py
+++ script.py
@@ -100,6 +100,7 @@ def afq_this(subject):
 
 subject_list = ["01", "02"]
 t = afq_this(subject=subject_list, cache_dir=cache_dir_tmp).split("subject")
+
 with pydra.Submitter(plugin="slurm",
-                     sbatch_args=f"-J pyafq-sub-{subject} -p gpu-a40 -A escience --mem=58G --time=10:00:00 -o /gscratch/escience/arokem/logs/pyafq-sub-{subject}.out -e /gscratch/escience/arokem/logs/pyafq-sub-{subject}.err --mail-user=arokem@uw.edu --mail-type=ALL") as sub:
+                     sbatch_args=f"-J pyafq -p gpu-a40 -A escience --mem=58G --time=10:00:00 -o /gscratch/escience/arokem/logs/pyafq.out -e /gscratch/escience/arokem/logs/pyafq.err --mail-user=arokem@uw.edu --mail-type=ALL") as sub:
     sub(runnable=t)
